c4d2c78f9018a862f583486b3328dd23013c7a0e
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-01-23 22:59:14 +0200

    eka

---
+++ los-numeros/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Numeros from './util/Numeros'
+
+const App = () => (
+  <div>
+    {Numeros.eword}
+    Hello World!
+  </div>
+)
+
+export default App;
---
+++ los-numeros/src/util/Numeros.js
@@ -0,0 +1,223 @@
+const Numeros = [
+    {
+        id: 0,
+        word: 'cero'
+    },
+    {
+        id: 1,
+        word: 'uno'
+    },
+    {
+        id: 2,
+        word: 'dos'
+    },
+    {
+        id: 3,
+        word: 'tres'
+    },
+    {
+        id: 4,
+        word: 'cuatro'
+    },
+    {
+        id: 5,
+        word: 'cinco'
+    },
+    {
+        id: 6,
+        word: 'seis'
+    },
+    {
+        id: 7,
+        word: 'siete'
+    },
+    {
+        id: 8,
+        word: 'ocho'
+    },
+    {
+        id: 9,
+        word: 'nueve'
+    },
+    {
+        id: 10,
+        word: 'diez'
+    },
+    {
+        id: 11,
+        word: 'once'
+    },
+    {
+        id: 12,
+        word: 'doce'
+    },
+    {
+        id: 13,
+        word: 'trece'
+    },
+    {
+        id: 14,
+        word: 'catorce'
+    },
+    {
+        id: 15,
+        word: 'quince'
+    },
+    {
+        id: 16,
+        word: 'dieciseis'
+    },
+    {
+        id: 17,
+        word: 'diecisiete'
+    },
+    {
+        id: 18,
+        word: 'dieciocho'
+    },
+    {
+        id: 19,
+        word: 'diecinueve'
+    },
+    {
+        id: 20,
+        word: 'veinte'
+    },
+    {
+        id: 21,
+        word: 'veintiuno'
+    },
+    {
+        id: 22,
+        word: 'veintidos'
+    },
+    {
+        id: 23,
+        word: 'veintitres'
+    },
+    {
+        id: 24,
+        word: 'veinticuatro'
+    },
+    {
+        id: 25,
+        word: 'veinticinco'
+    },
+    {
+        id: 26,
+        word: 'veintiseis'
+    },
+    {
+        id: 27,
+        word: 'veintisiete'
+    },
+    {
+        id: 28,
+        word: 'veintiocho'
+    },
+    {
+        id: 29,
+        word: 'veintinueve'
+    },
+    {
+        id: 30,
+        word: 'treinta'
+    },
+    {
+        id: 31,
+        word: 'treinta y uno'
+    },
+    {
+        id: 32,
+        word: 'treinta y dos'
+    },
+    {
+        id: 33,
+        word: 'treinta y tres'
+    },
+    {
+        id: 34,
+        word: 'treinta y cuatro'
+    },
+    {
+        id: 35,
+        word: 'treinta y cinco'
+    },
+    {
+        id: 36,
+        word: 'treinta y seis'
+    },
+    {
+        id: 37,
+        word: 'treinta y siete'
+    },
+    {
+        id: 38,
+        word: 'treinta y ocho'
+    },
+    {
+        id: 39,
+        word: 'treinta y nueve'
+    },
+    {
+        id: 40,
+        word: 'cuarenta'
+    },
+    {
+        id: 41,
+        word: 'cuarenta y uno'
+    },
+    {
+        id: 42,
+        word: 'cuarenta y dos'
+    },
+    {
+        id: 3,
+        word: 'tres'
+    },
+    {
+        id: 4,
+        word: 'cuatro'
+    },
+    {
+        id: 5,
+        word: 'cinco'
+    },
+    {
+        id: 6,
+        word: 'seis'
+    },
+    {
+        id: 7,
+        word: 'siete'
+    },
+    {
+        id: 8,
+        word: 'ocho'
+    },
+    {
+        id: 9,
+        word: 'nueve'
+    },
+    {
+        id: 10,
+        word: 'diez'
+    }
+]
+
+export default Numeros
+
+31	
+treinta y uno
+42	
+cuarenta y dos
+53	
+cincuenta y tres
+64	
+sesenta y cuatro
+75	
+setenta y cinco
+86	
+ochenta y seis
+97	
+noventa y siete 
